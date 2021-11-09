62d2df61515d9572dd49636ba86018b288257dc9
okay java <okaycomputing@gmail.com>
2021-11-09 15:29:56 -0500

    h2

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,4 @@
+INSERT INTO TBL_PLAYERS (first_name, last_name, email)
+VALUES  ('rishabh', 'pant', 'r.pant@cricket.com'),
+  ('kl', 'rahul', 'klrahul@cricket.com'),
+  ('yuvraj', 'singh', 'yuvraj@cricket.com');
